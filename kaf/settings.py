8475410af2eb33347e76691ca203438d56ae247c
ahmedbfci <ahmedomar.bfci@gmail.com>
2023-09-12 08:45:31 +0300

    Update settings.py

--- kaf/settings.py
+++ kaf/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wuq5*ne4s_u*ml**p3f%@$^=i1%!w^2w_1pvg(!7ooahu-dwqc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kaf-api.onrender.com']
 
 
 # Application definition
