907689ea73a6ee4e0a99454eb4d3b8fe7b836b87
ahmedbfci <ahmedomar.bfci@gmail.com>
2023-09-12 18:55:30 +0300

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -13,7 +13,5 @@ urlpatterns = [
     path('list/', UserListApiView.as_view()),
     path('search/like/', UserSearchLikeApiView.as_view()),
     path('search/exactly/', UserSearchExactlyApiView.as_view()),
-    path('uplod/', uploadApiView.as_view()),
-    #path('api/<str:user_name>/', TodoDetailApiView.as_view()),
-    #path("", views.index, name="index"),
+    path('upload/', uploadApiView.as_view())
 ]
